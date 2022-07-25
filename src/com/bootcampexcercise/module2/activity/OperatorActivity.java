23bd5b767cf7b51e03553ee9db993f4388c9571c
Neringa Šleževičienė <faulttester@gmail.com>
2022-07-25 16:26:35 +0300

    Create OperatorActivity.java

---
+++ src/com/bootcampexcercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,12 @@
+package com.bootcampexcercise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String []args) {
+        int a = 9;
+        int b = 10;
+        System.out.println("Value of a is " + a);
+        System.out.println("Value of b is " + b);
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
