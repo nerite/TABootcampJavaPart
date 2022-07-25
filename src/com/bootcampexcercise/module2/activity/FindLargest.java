a438e16d1b2cf04cdaaa91e37306e916e715afb1
Neringa Šleževičienė <faulttester@gmail.com>
2022-07-25 17:01:03 +0300

    Create FindLargest.java

---
+++ src/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -0,0 +1,13 @@
+package com.bootcampexcercise.module2.activity;
+
+public class FindLargest {
+    public static void main(String []args) {
+        int a = 25;
+        int b = 3;
+        if(a>b){
+            System.out.println("a is greater then b");
+        }else {
+            System.out.println("a is greater then b");
+        }
+    }
+}
