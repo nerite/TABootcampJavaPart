8e70ee668ccac513d4ae683487513c98351c9954
Neringa Šleževičienė <faulttester@gmail.com>
2022-07-25 14:47:26 +0300

    Update VariableAssignmentActivity.java

--- src/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
+++ src/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
@@ -3,8 +3,9 @@ package com.bootcampexcercise.module2.activity;
 public class VariableAssignmentActivity {
     public static void main(String []args) {
         int i=2;
+        System.out.println("Default value = "+ i );
         i=5;
-        System.out.println(i);
+        System.out.println("Updated value = " + i);
 
     }
 }
