2b2681a767b4848c406959a5bb61b66d30774364
Neringa Šleževičienė <faulttester@gmail.com>
2022-07-25 16:42:19 +0300

    Update OperatorActivity.java

--- src/com/bootcampexcercise/module2/activity/OperatorActivity.java
+++ src/com/bootcampexcercise/module2/activity/OperatorActivity.java
@@ -2,11 +2,14 @@ package com.bootcampexcercise.module2.activity;
 
 public class OperatorActivity {
     public static void main(String []args) {
-        int a = 9;
-        int b = 10;
+        int a = 12;
+        int b = 2;
         System.out.println("Value of a is " + a);
         System.out.println("Value of b is " + b);
-        int sum = a + b;
-        System.out.println("Sum = " + sum);
+        System.out.println("Addition = " + (a+b));
+        System.out.println("Subtraction = " + (a-b));
+        System.out.println("Division = " + (a/b));
+        System.out.println("Multiplication = " + (a*b));
+        System.out.println("Modulo = " + (a%b));
     }
 }
