0a6778eb18726691925c4320bc108294c8f70e19
Neringa Šleževičienė <faulttester@gmail.com>
2022-07-25 14:35:01 +0300

    Create VariableAssignmentActivity.java

---
+++ src/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,10 @@
+package com.bootcampexcercise.module2.activity;
+
+public class VariableAssignmentActivity {
+    public static void main(String []args) {
+        int i=2;
+        i=5;
+        System.out.println(i);
+
+    }
+}
