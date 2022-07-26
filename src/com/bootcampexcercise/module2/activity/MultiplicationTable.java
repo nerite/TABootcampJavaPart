398289634cf4c03a7fd86dc985b9ab65b569de87
Neringa Šleževičienė <faulttester@gmail.com>
2022-07-26 12:02:02 +0300

    Update MultiplicationTable.java

--- src/com/bootcampexcercise/module2/activity/MultiplicationTable.java
+++ src/com/bootcampexcercise/module2/activity/MultiplicationTable.java
@@ -12,7 +12,7 @@ public class MultiplicationTable {
         // Table from 11 to 20
         for (int i = 11; i <= 20; i++) {
             System.out.println("*** Table of "+ i + " ***");
-            for (int j = 11; j <= 20; j++) {
+            for (int j = 1; j <= 10; j++) {
                 System.out.println(i + "X" + j +" = " + (i*j));
             }
         }
