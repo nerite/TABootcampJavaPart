d8cccd34457625e76c54b71d493e53df44990448
Neringa Šleževičienė <faulttester@gmail.com>
2022-07-27 21:32:30 +0300

    Create InterfaceSample.java

---
+++ src/com/bootcampexcercise/module5/sample/interfaceSample/InterfaceSample.java
@@ -0,0 +1,15 @@
+package com.bootcampexcercise.module5.sample.interfaceSample;
+
+public class InterfaceSample {
+    public static void main(String[] args) {
+
+        Lion l = new Lion();
+        Bird b = new Bird();
+
+        l.move();
+        b.move();
+
+
+    }
+
+}
