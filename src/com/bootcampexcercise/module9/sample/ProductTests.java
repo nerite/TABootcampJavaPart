04d2e4bc1b1bf77854c24cff273c1e584648a764
Neringa Šleževičienė <faulttester@gmail.com>
2022-07-28 21:47:58 +0300

    Create ProductTests.java

---
+++ src/com/bootcampexcercise/module9/sample/ProductTests.java
@@ -0,0 +1,28 @@
+package com.bootcampexcercise.module9.sample;
+
+import junit.framework.TestCase;
+
+public class ProductTests extends TestCase {
+
+    private Product product;
+
+    protected void setUp() throws Exception {
+        product = new Product();
+    }
+
+    public void testSetAndGetDescription() {
+        String testDescription = "aDescription";
+        assertNull(product.getDescription());
+        product.setDescription(testDescription);
+        assertEquals(testDescription, product.getDescription());
+    }
+
+    public void testSetAndGetPrice() {
+        double testPrice = 100.00;
+        assertEquals(null, product.getPrice());
+        assertNull(product.getPrice());
+        product.setPrice(testPrice);
+        assertEquals(testPrice, product.getPrice());
+    }
+
+}
